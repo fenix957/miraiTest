18056170ab3cf41dc707a0b29e4cca9aa44364dd
max <max@f4z.ru>
2022-09-28 22:55:39 +0300

    Add Example Db configuration and DB

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * App configuration
+ */
+$config = [];
+$config["mysql"] = [];
+$config["mysql"]["host"] = 'fenixfxv.beget.tech';
+$config["mysql"]["port"] = 3306;
+$config["mysql"]["username"] = 'fenixfxv_mirai';
+$config["mysql"]["password"] = 'fenixfxv.beget.techMirai';
+$config["mysql"]["db"] = 'fenixfxv_mirai';
+
+$config['api'] = [];
+$config['api']['key']= 'Y9YGXWETZ5FW';
+$config['api']["endpoint"] = 'http://api.timezonedb.com/v2.1/';
+
+return $config;
+
